2d4d5c0ff98d6ed0cba64aa490fde7c402bcd858
Ferraronp <avzal-ar@mail.ru>
2020-11-13 18:04:51 +0300

    Git and yellow elipses

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.draw = False
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.draw = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.draw:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ocr(qp)
+            qp.end()
+        self.draw = False
+
+    def draw_ocr(self, qp):
+        for i in range(randint(1, 10)):
+            qp.setBrush(QColor(255, 255, 0))
+            x, y, r = randint(100, 400), randint(100, 400), randint(25, 100)
+            qp.drawEllipse(x, y, r, r)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
